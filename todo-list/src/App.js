dd257c3264cb4ae8fa7ea39c71a24521c1dbacf0
Noctis <edisonnoctis@hotmail.com>
2025-09-07 08:00:49 -0500

    Commit Todo-list

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 
+import TodoList from './todo-list';
 function App() {
   return (
     <div className="App">
-      
+
+        <TodoList/>
     </div>
   );
 }
--- todo-list/src/todo-list.jsx
+++ todo-list/src/todo-list.jsx
@@ -1,7 +1,41 @@
 import React, { useState } from "react";
+import { FaTrash } from "react-icons/fa";
+
 
 function TodoList (){
     const [input, setInput]= useState ("")
+    const [lista, setLista] = useState([])
 
+    const eliminarTarea = (index) => {
+    const nuevaLista = lista.filter((item, idx) => idx !== index); 
+    setLista(nuevaLista); 
+  };
 
+    return(
+        <div>
+            <h1> Todo List</h1>
+            <input type="text" value={input} onChange={(e) => setInput(e.target.value)}
+            placeholder="Escribe algo para tu lista" />
+
+            <button onClick={()=> {
+             if(input.trim() !== ""){
+                setLista([...lista, input])
+                setInput("")
+             }   
+            }}>
+                Agregar Tarea
+            </button>
+
+            <ul>
+                {lista.map((item, index) =>(<li key={index}>{item}   <FaTrash
+              onClick={() => eliminarTarea(index)} // Llama a la función de eliminar pasando el índice
+              style={{ cursor: "pointer", marginLeft: "10px", color: "silver" }}
+            /> </li>)) }
+               
+            </ul>
+        </div>
+        
+    )
 }
+
+export default TodoList;
